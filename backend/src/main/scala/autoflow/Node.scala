0e64be48fdf4e9bcb038b0526a2d256b3a182568
sthomer <steven.t.homer@gmail.com>
2018-02-24 06:06:16 +0100

    naive and learning agents

--- backend/src/main/scala/autoflow/Node.scala
+++ backend/src/main/scala/autoflow/Node.scala
@@ -1,6 +1,8 @@
 package autoflow
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import autoflow.Action.{Coloring, Green, Red}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.util.Random
@@ -10,8 +12,7 @@ trait Action
 object Action {
   case object Green extends Action
   case object Red extends Action
-  case class Green(i: Int) extends Action
-  case class Red(i: Int) extends Action
+  case class Coloring(green: List[Int], red: List[Int])
 }
 
 trait StateAttribute
@@ -21,28 +22,32 @@ object StateAttribute {
 }
 
 case class StateComponent(attribute: StateAttribute, face: Int, value: Int)
-case class State(attrs: Set[StateComponent])
+case class State(comps: Set[StateComponent])
 
 trait NodeAgent {
+  this: NodeArtifact with NodeActor =>
+
   val a = 0.1 // low variance
   val g = 0.9 // far-sighted
   val t = 0.1 // greedy
 
   type Quality = Double
-  var state: State
+  var state: State = State(Set.empty[StateComponent])
+  var next: State = State(Set.empty[StateComponent])
+  var coloring: Coloring
 
-  var q: Map[State, Map[Action, Quality]] = Map.empty
+  var q: Map[State, Map[Coloring, Quality]] = Map.empty
 
-  def update(next: State, action: Action): Unit = {
-    val actions: Map[Action, Quality] =
-      q.getOrElse(state, Map.empty[Action, Quality])
+  def update(coloring: Coloring): Unit = {
+    val colorings: Map[Coloring, Quality] =
+      q.getOrElse(state, Map.empty[Coloring, Quality])
     val quality: Quality =
-      actions.getOrElse(action, 0.0)
+      colorings.getOrElse(coloring, 0.0)
     val prime: Quality =
       (1 - a) * quality + a * (reward(next) + g * optimal(next))
 
-    actions(action) = prime
-    q = q + (state -> actions)
+    colorings(coloring) = prime
+    q = q + (state -> colorings)
 
     state = next
   }
@@ -50,97 +55,180 @@ trait NodeAgent {
   def exp(value: Double): Double =
     math.pow(math.E, value / t)
 
-  def pr(actions: Map[Action, Quality], action: Action): Double =
-    exp(actions(action)) / actions.keys.map(actions(_)).sum
+  def pr(colorings: Map[Coloring, Quality], coloring: Coloring): Double =
+    exp(colorings(coloring)) /
+      colorings.values.map(exp).sum
 
-  def choose(state: State): Action = {
+  def choose(): Coloring = {
     val r = Random nextDouble
     var sum = 0.0
-    val actions = q(state)
-    for ((action, quality) <- actions) yield {
-      sum += pr(actions, action)
-      if (r <= sum) return action
-    }
-    actions.head._1 // this should never happen
-  }
 
-//  def act(state: State): Action = {
-//    val actions: Map[Action, Quality] =
-//      q.getOrElse(state, Map.empty[Action, Quality])
-//
-//  }
+    val colorings = q.getOrElse(state, Map.empty + (coloring -> 0.0) +
+      (coloring match {
+        case Coloring(green, red) => Coloring(red, green) -> 0.0 }))
+    for ((coloring, quality) <- colorings) yield {
+      sum += pr(colorings, coloring)
+      if (r <= sum) {
+        update(coloring)
+        return coloring
+      }
+    }
+    colorings.head._1 // this should never happen
+  }
 
   def reward(state: State): Double =
-    if (state.attrs.exists(c => c.value != 0.0)) 0.0 else 100.0
+    if (state.comps.exists(c => c.value != 0.0)) 0.0 else 100.0
 
   def optimal(state: State): Double = {
-    val actions: Map[Action, Quality] =
-      q.getOrElse(state, Map.empty[Action, Quality])
-    (0.0 /: actions) (_ max _._2)
+    val colorings: Map[Coloring, Quality] =
+      q.getOrElse(state, Map.empty[Coloring, Quality])
+    (0.0 /: colorings) (_ max _._2)
   }
 
-  // should be in aggregator?
-  //  def updateState(attr: StateAttr, value: Double): Unit = {
-  //    state = state + (attr -> value)
-  //  }
+  def updateNext(comp: StateComponent): Unit =
+    next = next.copy(comps = next.comps + comp)
+}
+
+trait NodeAgentNaive {
+  this: NodeArtifact with NodeActor =>
+
+  var coloring: Coloring
+
+  def choose(): Coloring = { coloring match {
+    case Coloring(green, red) => coloring = Coloring(red, green)
+  }
+    coloring
+  }
 }
 
 // Node
-trait Node {
+trait NodeArtifact {
+
   val id: Int
   val latitude: Double
   val longitude: Double
-  val faces: List[Face]
 }
 
-trait NodeActor extends Node with Actor with ActorLogging {
+trait NodeActor extends Actor with ActorLogging {
+  this: NodeArtifact with NodeAgent =>
+  val faces: List[ActorRef]
 
   context.system.scheduler.schedule(
     initialDelay = 5 seconds, interval = 5 seconds, self, Tick)
 
   def receive = {
-    case Tick => ??? // act
-    case emit: Emit => ??? // updateState
-    case _ => log.info("node")
+    case Tick =>
+      val choice = choose()
+      choice.green.foreach(faces(_) ! Green)
+      choice.red.foreach(faces(_) ! Red)
+    case comp: StateComponent => updateNext(comp)
   }
+}
+
+case class Node(faces: List[ActorRef],
+                id: Int,
+                latitude: Double,
+                longitude: Double,
+                var coloring: Coloring
+               ) extends NodeArtifact with NodeActor with NodeAgent
+
+object Node {
+  def props(faces: List[ActorRef],
+            id: Int,
+            latitude: Double,
+            longitude: Double,
+            coloring: Coloring): Props =
+    Props(classOf[Node], faces, id, latitude, longitude, coloring)
 }
 
 // Face
-trait Face {
-  def emit: Double
-  val in: Option[InGate]
-  val out: Option[OutGate]
+trait FaceArtifact {
+//  val in: Option[InGate]
+//  val out: Option[OutGate]
+  var light: Action
+  var process = List.empty[Int]  // each Int is a car with wait time
 }
 
-trait FaceActor extends Face with Actor with ActorLogging {
-  val node: ActorRef
-
-  def receive = {
-    case emit: Emit => node ! emit
-    case _ => log.info("face")
-  }
-}
-
-// Sensor
-trait Sensor {
-  def emit: Double
-}
-
-trait SensorActor extends Sensor with Actor with ActorLogging {
-  val face: ActorRef
+trait FaceActor extends Actor with ActorLogging {
+  this: FaceArtifact =>
+  val sensors: List[ActorRef]
 
   context.system.scheduler.schedule(
     initialDelay = 1 seconds, interval = 1 seconds, self, Tick)
 
+  context.system.scheduler.schedule(
+    initialDelay = 3 seconds, interval = 3 seconds, self, Queue)
+
   def receive = {
-    case Tick => face ! Emit(emit)
-    case _ => log.info("sensor")
+    case Tick =>
+      process = if (light == Green) process.drop(1) else process
+      process = process.map(_ + 1)
+      sensors.foreach(_ ! Line(process))
+    case Queue =>
+      process = (1 :: process.reverse).reverse
+    case Green => light = Green
+    case Red => light = Red
+    case comp: StateComponent =>
+      log.info(comp.toString())
+      context.actorSelection("/user/node") ! comp
   }
+}
+
+case class Face(sensors: List[ActorRef],
+//                in: InGate,
+//                out: OutGate,
+                var light: Action
+               ) extends FaceArtifact with FaceActor
+
+object Face {
+  def props(sensors: List[ActorRef], light: Action): Props =
+    Props(classOf[Face], sensors, /*Some(InGate()), Some(OutGate()),*/ light)
+}
+
+
+// Sensor
+trait SensorArtifact {
+  val faceIndex: Int
+  def emit(line: List[Int]): Int
+  val attr: StateAttribute
+}
+
+trait SensorActor extends Actor with ActorLogging {
+  this: SensorArtifact =>
+
+  def receive = {
+    case Line(line: List[Int]) =>
+      sender ! StateComponent(attr, faceIndex, emit(line))
+  }
+}
+
+case class LoadSensor(faceIndex: Int)
+  extends SensorArtifact with SensorActor {
+
+  def emit(line: List[Int]): Int = line.length
+  val attr: StateAttribute = StateAttribute.Load
+}
+
+object LoadSensor {
+  def props(faceIndex: Int): Props = Props(classOf[LoadSensor], faceIndex)
+}
+
+case class WaitSensor(faceIndex: Int)
+  extends SensorArtifact with SensorActor {
+
+  def emit(line: List[Int]): Int = line.sum
+  val attr: StateAttribute = StateAttribute.Wait
+}
+
+object WaitSensor {
+  def props(faceIndex: Int): Props = Props(classOf[WaitSensor], faceIndex)
 }
 
 case class InGate()
 case class OutGate()
 
 // Specific emit messages for each type of sensor ???
-case class Emit(signal: Double)
+case class Emit(face: Int, signal: Double)
+case class Line(line: List[Int])
 object Tick
+object Queue
--- backend/src/main/scala/com/lightbend/akka/http/sample/QuickstartServer.scala
+++ backend/src/main/scala/com/lightbend/akka/http/sample/QuickstartServer.scala
@@ -2,36 +2,56 @@ package com.lightbend.akka.http.sample
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-
-import akka.actor.{ ActorRef, ActorSystem }
+import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import autoflow.Action.{Coloring, Green, Red}
+import autoflow._
 
 //#main-class
-object QuickstartServer extends App with UserRoutes {
+object QuickstartServer extends App { //with UserRoutes {
 
   // set up ActorSystem and other dependencies here
   //#main-class
   //#server-bootstrapping
-  implicit val system: ActorSystem = ActorSystem("helloAkkaHttpServer")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val system: ActorSystem = ActorSystem("simulation")
+//  implicit val materializer: ActorMaterializer = ActorMaterializer()
   //#server-bootstrapping
 
-  val userRegistryActor: ActorRef = system.actorOf(UserRegistryActor.props, "userRegistryActor")
+//  val userRegistryActor: ActorRef = system.actorOf(UserRegistryActor.props, "userRegistryActor")
 
   //#main-class
   // from the UserRoutes trait
-  lazy val routes: Route = userRoutes
+//  lazy val routes: Route = userRoutes
   //#main-class
 
   //#http-server
-  Http().bindAndHandle(routes, "localhost", 8080)
+//  Http().bindAndHandle(routes, "localhost", 8080)
 
-  println(s"Server online at http://localhost:8080/")
+//  println(s"Server online at http://localhost:8080/")
 
-  Await.result(system.whenTerminated, Duration.Inf)
+//  Await.result(system.whenTerminated, Duration.Inf)
   //#http-server
   //#main-class
+
+
+  val load0 = system.actorOf(LoadSensor.props(0), "load0")
+  val wait0 = system.actorOf(WaitSensor.props(0), "wait0")
+  val face0 = system.actorOf(Face.props(List(load0, wait0), Green))
+  val load1 = system.actorOf(LoadSensor.props(1), "load1")
+  val wait1 = system.actorOf(WaitSensor.props(1), "wait1")
+  val face1 = system.actorOf(Face.props(List(load1, wait1), Red))
+  val load2 = system.actorOf(LoadSensor.props(2), "load2")
+  val wait2 = system.actorOf(WaitSensor.props(2), "wait2")
+  val face2 = system.actorOf(Face.props(List(load2, wait2), Green))
+  val load3 = system.actorOf(LoadSensor.props(3), "load3")
+  val wait3 = system.actorOf(WaitSensor.props(3), "wait3")
+  val face3 = system.actorOf(Face.props(List(load3, wait3), Red))
+  val faces: List[ActorRef] = List(face0, face1, face2, face3)
+  val node = system.actorOf(Node.props(faces,
+    12345, 4.345, 50.432, Coloring(List(0,2), List(1,3))), "node")
+
+
 }
 //#main-class
